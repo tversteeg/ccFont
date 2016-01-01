d9dea7ea604643fd92cdc7cd8584d76e230579f9
Thomas Versteeg <thomasversteeg@gmx.com>
2016-01-01 09:31:17 +0700

    Build script now builds library and tests

--- test/test.c
+++ test/test.c
@@ -1,6 +1,64 @@
 #include <ccFont/ccFont.h>
 
+#include <ccore/display.h>
+#include <ccore/window.h>
+#include <ccore/opengl.h>
+#include <ccore/time.h>
+
+#ifdef WINDOWS
+#include <gl/GL.h>
+#else
+#include <GL/glew.h>
+#endif
+
+#define WIDTH 400
+#define HEIGHT 32
+
+GLuint gltex;
+
 int main(int argc, char **argv)
 {
+	ccDisplayInitialize();
+
+	ccWindowCreate((ccRect){0, 0, WIDTH, HEIGHT}, "ccFont test", 0);
+	ccWindowMouseSetCursor(CC_CURSOR_NONE);
+
+	ccGLContextBind();
+
+	glEnable(GL_TEXTURE_2D);
+	glDisable(GL_DEPTH_TEST);
+	glDisable(GL_LIGHTING);
+
+	glBindTexture(GL_TEXTURE_2D, 0);
+	glGenTextures(1, &gltex);
+	glBindTexture(GL_TEXTURE_2D, gltex);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+
+	bool loop = true;
+	while(loop){
+		while(ccWindowEventPoll()){
+			ccEvent event = ccWindowEventGet();
+			switch(event.type){
+				case CC_EVENT_WINDOW_QUIT:
+					loop = false;
+					break;
+				case CC_EVENT_KEY_DOWN:
+					if(event.keyCode == CC_KEY_ESCAPE){
+						loop = false;
+					}
+					break;
+				default: break;
+			}
+		}
+
+		ccGLBuffersSwap();
+
+		ccTimeDelay(6);
+	}
+
+	ccFree();
 	return 0;
 }
