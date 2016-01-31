a0cee53bc5dd85264644ddff8e424242efdc6b73
Thomas Versteeg <thomasversteeg@gmx.com>
2016-01-31 16:31:57 +0700

    Refactor rendering script and fix blitting error

--- src/ccFont/ccFont.c
+++ src/ccFont/ccFont.c
@@ -24,6 +24,93 @@ static inline void byteToBools(uint8_t byte, uint8_t *b, size_t len)
 	}
 }
 
+static int _ccfBlitChar(const ccfFont *font, char ch, int x, int y, const float *color, GLsizei width, GLsizei height, GLenum format, GLenum type, GLvoid *data) 
+{
+	int c = ch - font->gstart;
+	if(c < 0 || c > font->gnum){
+		return -3;
+	}
+
+	int xstart = c * font->gwidth;
+
+	char bits = 0;
+	switch(format){
+		case GL_RGBA: 
+		case GL_BGRA: 
+			bits = 4;
+			break;
+		case GL_RGB: 
+		case GL_BGR: 
+			bits = 3;
+			break;
+		case GL_RG: 
+			bits = 2;
+			break;
+		case GL_RED: 
+			bits = 1;
+		break; 
+	}
+
+#define _CCF_SET_CHANNEL(target, bit, ti, ci) \
+	target[ti] = target[ti] * (1 - bit) + color[ci] * bit;
+
+#define _CCF_LOOP_PIXELS(_CCF_PIXEL_TYPE) \
+{\
+	for(int i = 0; i < font->gheight; i++){ \
+		for(int j = 0; j < font->gwidth; j++){ \
+			_CCF_PIXEL_TYPE *target = (_CCF_PIXEL_TYPE*)data + (j + x + (i + y) * width) * bits; \
+			uint8_t bit = font->bits[xstart + j + i * font->width]; \
+			switch(format){ \
+				case GL_RGBA: \
+					_CCF_SET_CHANNEL(target, bit, 3, 3); \
+				case GL_RGB: \
+					_CCF_SET_CHANNEL(target, bit, 2, 2); \
+				case GL_RG: \
+					_CCF_SET_CHANNEL(target, bit, 1, 1); \
+				case GL_RED: \
+					_CCF_SET_CHANNEL(target, bit, 0, 0); \
+					break; \
+				case GL_BGRA: \
+					_CCF_SET_CHANNEL(target, bit, 3, 3); \
+				case GL_BGR: \
+					_CCF_SET_CHANNEL(target, bit, 0, 2); \
+					_CCF_SET_CHANNEL(target, bit, 1, 1); \
+					_CCF_SET_CHANNEL(target, bit, 2, 0); \
+				break; \
+			}\
+		} \
+	} \
+}
+
+	switch(type){
+		case GL_UNSIGNED_BYTE:
+			_CCF_LOOP_PIXELS(unsigned char);
+			break;
+		case GL_BYTE:
+			_CCF_LOOP_PIXELS(char);
+			break;
+		case GL_UNSIGNED_SHORT:
+			_CCF_LOOP_PIXELS(unsigned short);
+			break;
+		case GL_SHORT:
+			_CCF_LOOP_PIXELS(short);
+			break;
+		case GL_UNSIGNED_INT:
+			_CCF_LOOP_PIXELS(unsigned int);
+			break;
+		case GL_INT:
+			_CCF_LOOP_PIXELS(int);
+			break;
+		case GL_FLOAT:
+			_CCF_LOOP_PIXELS(float);
+			break;
+	}
+
+#undef _CCF_LOOP_PIXELS
+
+	return 0;
+}
+
 void ccfFreeFont(ccfFont *font)
 {
 	free(font->bits);
@@ -177,250 +264,42 @@ void ccfTtfToFont(ccfFont *font, const void *ttfbin, int size, unsigned firstcha
 	}
 }
 
+int ccfGLTexBlitChar(const ccfFont *font, char c, const ccfFontConfiguration *config, GLsizei width, GLsizei height, GLenum format, GLenum type, GLvoid *data)
+{
+	//TODO wrap around
+	if(format != GL_RED && format != GL_RG && format != GL_RGB && format != GL_RGBA && format != GL_BGR && format != GL_BGRA){
+		return -1;
+	}
+	if(type != GL_UNSIGNED_BYTE && type != GL_BYTE && type != GL_UNSIGNED_SHORT && type != GL_SHORT && type != GL_UNSIGNED_INT && type != GL_INT && type != GL_FLOAT){
+		return -2;
+	}	
+
+	return _ccfBlitChar(font, c, config->x, config->y, config->color, width, height, format, type, data); 
+}
+
 int ccfGLTexBlitText(const ccfFont *font, const char *string, const ccfFontConfiguration *config, GLsizei width, GLsizei height, GLenum format, GLenum type, GLvoid *data)
 {
 	//TODO wrap around
-
-	// Horrible macro magic to make sure there is no 'if' inside the while loop
-#define _CCF_LOOP_PIXELS(_CCF_PIXEL_FUNC, _CCF_PIXEL_TYPE) \
-	{\
-		int i, len = strlen(string); \
-		for(i = 0; i < len; i++){ \
-			if(string[i] == ' '){ \
-				continue; \
-			} \
-			int c = string[i] - font->gstart; \
-			if(c < 0 || c > font->gnum){ \
-				return -3; \
-			} \
-			\
-			int xtstart = config->x + i * font->gwidth; \
-			int xfstart = c * font->gwidth; \
-			int y; \
-			for(y = 0; y < font->gheight; y++){ \
-				int x; \
-				for(x = 0; x < font->gwidth; x++){ \
-					_CCF_PIXEL_FUNC(_CCF_PIXEL_TYPE); \
-				} \
-			} \
-		} \
-	}
-
-#define _CCF_PIXEL_FUNC_RED(_CCF_PIXEL_TYPE) \
-	_CCF_PIXEL_TYPE *target = (_CCF_PIXEL_TYPE*)data + (config->y + y) * width + x + xtstart; \
-	*target = font->bits[y * font->width + x + xfstart] * config->color[0];
-
-#define _CCF_PIXEL_FUNC_RG(_CCF_PIXEL_TYPE) \
-	_CCF_PIXEL_TYPE *target = (_CCF_PIXEL_TYPE*)data + ((config->y + y) * width + x + xtstart) * 2; \
-	*target = font->bits[y * font->width + x + xfstart] * config->color[0]; \
-	*(target + 1) = font->bits[y * font->width + x + xfstart] * config->color[1];
-
-#define _CCF_PIXEL_FUNC_RGB(_CCF_PIXEL_TYPE) \
-	_CCF_PIXEL_TYPE *target = (_CCF_PIXEL_TYPE*)data + ((config->y + y) * width + x + xtstart) * 3; \
-	*target = font->bits[y * font->width + x + xfstart] * config->color[0]; \
-	*(target + 1) = font->bits[y * font->width + x + xfstart] * config->color[1]; \
-	*(target + 2) = font->bits[y * font->width + x + xfstart] * config->color[2];
-
-#define _CCF_PIXEL_FUNC_RGBA(_CCF_PIXEL_TYPE) \
-	_CCF_PIXEL_TYPE *target = (_CCF_PIXEL_TYPE*)data + ((config->y + y) * width + x + xtstart) * 4; \
-	*target = font->bits[y * font->width + x + xfstart] * config->color[0]; \
-	*(target + 1) = font->bits[y * font->width + x + xfstart] * config->color[1]; \
-	*(target + 2) = font->bits[y * font->width + x + xfstart] * config->color[2]; \
-	*(target + 3) = font->bits[y * font->width + x + xfstart] * config->color[3];
-
-#define _CCF_PIXEL_FUNC_BGR(_CCF_PIXEL_TYPE) \
-	_CCF_PIXEL_TYPE *target = (_CCF_PIXEL_TYPE*)data + ((config->y + y) * width + x + xtstart) * 3; \
-	*target = font->bits[y * font->width + x + xfstart] * config->color[2]; \
-	*(target + 1) = font->bits[y * font->width + x + xfstart] * config->color[1]; \
-	*(target + 2) = font->bits[y * font->width + x + xfstart] * config->color[0];
-
-#define _CCF_PIXEL_FUNC_BGRA(_CCF_PIXEL_TYPE) \
-	_CCF_PIXEL_TYPE *target = (_CCF_PIXEL_TYPE*)data + ((config->y + y) * width + x + xtstart) * 4; \
-	*target = font->bits[y * font->width + x + xfstart] * config->color[2]; \
-	*(target + 1) = font->bits[y * font->width + x + xfstart] * config->color[1]; \
-	*(target + 2) = font->bits[y * font->width + x + xfstart] * config->color[0]; \
-	*(target + 3) = font->bits[y * font->width + x + xfstart] * config->color[3];
-
-#define _CCF_SWITCH_FORMATS(_CCF_PIXEL_TYPE) \
-	switch(format){ \
-		case GL_RED: \
-								 _CCF_LOOP_PIXELS(_CCF_PIXEL_FUNC_RED, _CCF_PIXEL_TYPE); \
-		break; \
-		case GL_RG: \
-								_CCF_LOOP_PIXELS(_CCF_PIXEL_FUNC_RG, _CCF_PIXEL_TYPE); \
-		break; \
-		case GL_RGB: \
-								 _CCF_LOOP_PIXELS(_CCF_PIXEL_FUNC_RGB, _CCF_PIXEL_TYPE); \
-		break; \
-		case GL_RGBA: \
-									_CCF_LOOP_PIXELS(_CCF_PIXEL_FUNC_RGBA, _CCF_PIXEL_TYPE); \
-		break; \
-		case GL_BGR: \
-								 _CCF_LOOP_PIXELS(_CCF_PIXEL_FUNC_BGR, _CCF_PIXEL_TYPE); \
-		break; \
-		case GL_BGRA: \
-									_CCF_LOOP_PIXELS(_CCF_PIXEL_FUNC_BGRA, _CCF_PIXEL_TYPE); \
-		break; \
-	}
-
 	if(format != GL_RED && format != GL_RG && format != GL_RGB && format != GL_RGBA && format != GL_BGR && format != GL_BGRA){
 		return -1;
 	}
 	if(type != GL_UNSIGNED_BYTE && type != GL_BYTE && type != GL_UNSIGNED_SHORT && type != GL_SHORT && type != GL_UNSIGNED_INT && type != GL_INT && type != GL_FLOAT){
 		return -2;
+	}	
+
+	int x = config->x;
+	int y = config->y;
+	for(const char *c = string; *c != '\0'; c++){
+		x += font->gwidth;
+		if(*c == '\n'){
+			x = config->x;
+			y += font->gheight;
+		}
+		int err = _ccfBlitChar(font, *c, x, y, config->color, width, height, format, type, data); 
+		if(err < 0){
+			return err;
+		}
 	}
-
-	switch(type){
-		case GL_UNSIGNED_BYTE:
-			_CCF_SWITCH_FORMATS(unsigned char);
-			break;
-		case GL_BYTE:
-			_CCF_SWITCH_FORMATS(char);
-			break;
-		case GL_UNSIGNED_SHORT:
-			_CCF_SWITCH_FORMATS(unsigned short);
-			break;
-		case GL_SHORT:
-			_CCF_SWITCH_FORMATS(short);
-			break;
-		case GL_UNSIGNED_INT:
-			_CCF_SWITCH_FORMATS(unsigned int);
-			break;
-		case GL_INT:
-			_CCF_SWITCH_FORMATS(int);
-			break;
-		case GL_FLOAT:
-			_CCF_SWITCH_FORMATS(float);
-			break;
-	}
-
-#undef _CCF_LOOP_PIXELS
-#undef _CCF_PIXEL_FUNC_RED
-#undef _CCF_PIXEL_FUNC_RG
-#undef _CCF_PIXEL_FUNC_RGB
-#undef _CCF_PIXEL_FUNC_RGBA
-#undef _CCF_PIXEL_FUNC_BGR
-#undef _CCF_PIXEL_FUNC_BGRA
-#undef _CCF_SWITCH_FORMATS
-
-	return 0;
-}
-
-int ccfGLTexBlitChar(const ccfFont *font, char ch, const ccfFontConfiguration *config, GLsizei width, GLsizei height, GLenum format, GLenum type, GLvoid *data) 
-{
-#define _CCF_LOOP_PIXELS(_CCF_PIXEL_FUNC, _CCF_PIXEL_TYPE) \
-{\
-	int c = ch - font->gstart; \
-	if(c < 0 || c > font->gnum){ \
-		return -3; \
-	} \
-	int xtstart = config->x; \
-	int xfstart = c * font->gwidth; \
-	int y; \
-	for(y = 0; y < font->gheight; y++){ \
-		int x; \
-		for(x = 0; x < font->gwidth; x++){ \
-			_CCF_PIXEL_FUNC(_CCF_PIXEL_TYPE); \
-		} \
-	}\
-}
-
-#define _CCF_PIXEL_FUNC_RED(_CCF_PIXEL_TYPE) \
-	_CCF_PIXEL_TYPE *target = (_CCF_PIXEL_TYPE*)data + (config->y + y) * width + x + xtstart; \
-	*target = font->bits[y * font->width + x + xfstart] * config->color[0];
-
-#define _CCF_PIXEL_FUNC_RG(_CCF_PIXEL_TYPE) \
-	_CCF_PIXEL_TYPE *target = (_CCF_PIXEL_TYPE*)data + ((config->y + y) * width + x + xtstart) * 2; \
-	*target = font->bits[y * font->width + x + xfstart] * config->color[0]; \
-	*(target + 1) = font->bits[y * font->width + x + xfstart] * config->color[1];
-
-#define _CCF_PIXEL_FUNC_RGB(_CCF_PIXEL_TYPE) \
-	_CCF_PIXEL_TYPE *target = (_CCF_PIXEL_TYPE*)data + ((config->y + y) * width + x + xtstart) * 3; \
-	*target = font->bits[y * font->width + x + xfstart] * config->color[0]; \
-	*(target + 1) = font->bits[y * font->width + x + xfstart] * config->color[1]; \
-	*(target + 2) = font->bits[y * font->width + x + xfstart] * config->color[2];
-
-#define _CCF_PIXEL_FUNC_RGBA(_CCF_PIXEL_TYPE) \
-	_CCF_PIXEL_TYPE *target = (_CCF_PIXEL_TYPE*)data + ((config->y + y) * width + x + xtstart) * 4; \
-	*target = font->bits[y * font->width + x + xfstart] * config->color[0]; \
-	*(target + 1) = font->bits[y * font->width + x + xfstart] * config->color[1]; \
-	*(target + 2) = font->bits[y * font->width + x + xfstart] * config->color[2]; \
-	*(target + 3) = font->bits[y * font->width + x + xfstart] * config->color[3];
-
-#define _CCF_PIXEL_FUNC_BGR(_CCF_PIXEL_TYPE) \
-	_CCF_PIXEL_TYPE *target = (_CCF_PIXEL_TYPE*)data + ((config->y + y) * width + x + xtstart) * 3; \
-	*target = font->bits[y * font->width + x + xfstart] * config->color[2]; \
-	*(target + 1) = font->bits[y * font->width + x + xfstart] * config->color[1]; \
-	*(target + 2) = font->bits[y * font->width + x + xfstart] * config->color[0];
-
-#define _CCF_PIXEL_FUNC_BGRA(_CCF_PIXEL_TYPE) \
-	_CCF_PIXEL_TYPE *target = (_CCF_PIXEL_TYPE*)data + ((config->y + y) * width + x + xtstart) * 4; \
-	*target = font->bits[y * font->width + x + xfstart] * config->color[2]; \
-	*(target + 1) = font->bits[y * font->width + x + xfstart] * config->color[1]; \
-	*(target + 2) = font->bits[y * font->width + x + xfstart] * config->color[0]; \
-	*(target + 3) = font->bits[y * font->width + x + xfstart] * config->color[3];
-
-#define _CCF_SWITCH_FORMATS(_CCF_PIXEL_TYPE) \
-	switch(format){ \
-		case GL_RED: \
-								 _CCF_LOOP_PIXELS(_CCF_PIXEL_FUNC_RED, _CCF_PIXEL_TYPE); \
-		break; \
-		case GL_RG: \
-								_CCF_LOOP_PIXELS(_CCF_PIXEL_FUNC_RG, _CCF_PIXEL_TYPE); \
-		break; \
-		case GL_RGB: \
-								 _CCF_LOOP_PIXELS(_CCF_PIXEL_FUNC_RGB, _CCF_PIXEL_TYPE); \
-		break; \
-		case GL_RGBA: \
-									_CCF_LOOP_PIXELS(_CCF_PIXEL_FUNC_RGBA, _CCF_PIXEL_TYPE); \
-		break; \
-		case GL_BGR: \
-								 _CCF_LOOP_PIXELS(_CCF_PIXEL_FUNC_BGR, _CCF_PIXEL_TYPE); \
-		break; \
-		case GL_BGRA: \
-									_CCF_LOOP_PIXELS(_CCF_PIXEL_FUNC_BGRA, _CCF_PIXEL_TYPE); \
-		break; \
-	}
-
-	if(format != GL_RED && format != GL_RG && format != GL_RGB && format != GL_RGBA && format != GL_BGR && format != GL_BGRA){
-		return -1;
-	}
-	if(type != GL_UNSIGNED_BYTE && type != GL_BYTE && type != GL_UNSIGNED_SHORT && type != GL_SHORT && type != GL_UNSIGNED_INT && type != GL_INT && type != GL_FLOAT){
-		return -2;
-	}
-
-	switch(type){
-		case GL_UNSIGNED_BYTE:
-			_CCF_SWITCH_FORMATS(unsigned char);
-			break;
-		case GL_BYTE:
-			_CCF_SWITCH_FORMATS(char);
-			break;
-		case GL_UNSIGNED_SHORT:
-			_CCF_SWITCH_FORMATS(unsigned short);
-			break;
-		case GL_SHORT:
-			_CCF_SWITCH_FORMATS(short);
-			break;
-		case GL_UNSIGNED_INT:
-			_CCF_SWITCH_FORMATS(unsigned int);
-			break;
-		case GL_INT:
-			_CCF_SWITCH_FORMATS(int);
-			break;
-		case GL_FLOAT:
-			_CCF_SWITCH_FORMATS(float);
-			break;
-	}
-
-#undef _CCF_LOOP_PIXELS
-#undef _CCF_PIXEL_FUNC_RED
-#undef _CCF_PIXEL_FUNC_RG
-#undef _CCF_PIXEL_FUNC_RGB
-#undef _CCF_PIXEL_FUNC_RGBA
-#undef _CCF_PIXEL_FUNC_BGR
-#undef _CCF_PIXEL_FUNC_BGRA
-#undef _CCF_SWITCH_FORMATS
-
+	
 	return 0;
 }
